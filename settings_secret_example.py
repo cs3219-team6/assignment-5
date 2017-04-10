80c83d945832dbecaa5701b8341bdd8b69f9f8e5
Darren Wee <darren.wee@u.nus.edu>
2017-04-11 01:02:09 +0800

    add secret settings files and example

---
+++ settings_secret_example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+###############################
+### DO NOT COMMIT TO GITHUB ###
+###############################
+
+"""
+	Example file.
+
+	@@@ Rememeber to add this file to .gitignore when you put your real token in @@@
+"""
+
+TELEGRAM_API_TOKEN = '1234634650ijsdfgidjijij'
+GITHUB_API_TOKEN = '1234634650ijsdfgidjijij'
+
