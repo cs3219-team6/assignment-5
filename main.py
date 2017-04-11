19fc18bbfe7baf5aaefa99ae2f8710e1eddaa765
Tjong Anthony <tjonganthony@gmail.com>
2017-04-12 02:35:40 +0800

    add more guidance during start

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def parse_datetime(input):
         return None
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!\nYou can use /top_three /top_contributor and /last_commit")
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
